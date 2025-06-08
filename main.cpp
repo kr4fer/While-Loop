ceec65a34885aae81718034c2ad0f9af77722598
Sunty Eljohn <163456176+kr4fer@users.noreply.github.com>
2025-06-08 12:13:24 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a =1, b =10, c =2;
+
+    cout << "a. ";
+    while (a<=10) {
+          cout <<a<<"\n";
+          a++;
+    }
+    cout << "b. ";
+    while (b>=1) {
+        cout <<b<<"\n";
+        b--;
+    }
+    cout << "c. ";
+    while (c<=10) {
+        cout <<c<<"\n";
+        c++;
+    }
+    return 0;
+}
